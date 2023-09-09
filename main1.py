64ec320b54da184d4e8015fac1f827a9a3b1c1f4
Konstantin <k.m.klymenko@gmail.com>
2023-09-09 13:48:10 +0300

    Use functions

--- main1.py
+++ main1.py
@@ -1,2 +1,18 @@
-print("Hello World!")
-print("Hello Git")
+def ages():
+    age = input("How old are you? ")
+    try:
+        age = int(age)
+        if age >= 18:
+            print("You are adult.")
+        else:
+            print("You are infant")
+    except ValueError:
+        print(f"{age} is not a number")
+
+def name_data():
+    first_name = input("Type your name? ")
+    last_name = input("Type your soname? ")
+    print(f'Hello dear {first_name}!')
+
+name_data()
+ages()
