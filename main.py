68d06a0f618e1df8f9369c44b6c06306478644bf
Konstantin <k.m.klymenko@gmail.com>
2023-09-23 22:46:09 +0300

    fibonacci

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        qw = fibonacci(n - 1) + fibonacci(n - 2)
+        print (qw)
+        return qw
+        
+
+# Приклад використання:
+n = 5  # Задане число n
+result = fibonacci(n)
+print(f"{n}-те число ряду Фібоначчі: {result}")
 
 
 
