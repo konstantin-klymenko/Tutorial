825a55aba1fdd064ed37d4f7185e80e64673315b
Konstantin <k.m.klymenko@gmail.com>
2023-09-27 09:31:48 +0300

    Module 4

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-def fibonacci(n):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        qw = fibonacci(n - 1) + fibonacci(n - 2)
-        print (qw)
-        return qw
-        
+def amount_payment(payment):
+    sum = 0
+    for value in payment:
+        if value > 0:
+            sum = sum + value
+    return sum
 
-# Приклад використання:
-n = 5  # Задане число n
-result = fibonacci(n)
-print(f"{n}-те число ряду Фібоначчі: {result}")
-
+payment = [-100, 50, -20, 30, -10]
+result = amount_payment(payment)
+print(f"Сума платежу наприкінці місяця: {result}")
+  
 
 
 
